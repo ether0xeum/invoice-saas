d150f4d8e78d5b05c8f752298ad3917dbf31a34b
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-03-31 14:15:13 -0300

    feat: add to download invoice

--- app/components/InvoiceActions.tsx
+++ app/components/InvoiceActions.tsx
@@ -22,7 +22,7 @@ export function InvoiceActions({ id }: iAppProps) {
                     </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                    <Link href="">
+                    <Link href={`/api/invoice/${id}`} target="_blank">
                         <DownloadCloudIcon className="size-4 mr-2" /> Download Invoice
                     </Link>
                 </DropdownMenuItem>
